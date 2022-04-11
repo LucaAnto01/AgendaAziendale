9e82333661b5e1b71a19518d3b48e8429ab792b9
LucaAnto01 <luca.antognarelli@gmail.com>
2022-04-11 17:24:36 +0200

    Creazione form admin

---
+++ AgendaAziendale/Forms/FormAdmin.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace AgendaAziendale
+{
+    partial class FormAdmin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelTop = new System.Windows.Forms.Panel();
+            this.btChiudi = new System.Windows.Forms.Button();
+            this.panelSinistra = new System.Windows.Forms.Panel();
+            this.btLogout = new System.Windows.Forms.Button();
+            this.btGestioneUtenti = new System.Windows.Forms.Button();
+            this.panelCentro = new System.Windows.Forms.Panel();
+            this.panelTop.SuspendLayout();
+            this.panelSinistra.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelTop
+            // 
+            this.panelTop.BackColor = System.Drawing.Color.Gainsboro;
+            this.panelTop.Controls.Add(this.btChiudi);
+            this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTop.Location = new System.Drawing.Point(0, 0);
+            this.panelTop.Name = "panelTop";
+            this.panelTop.Size = new System.Drawing.Size(1000, 40);
+            this.panelTop.TabIndex = 3;
+            // 
+            // btChiudi
+            // 
+            this.btChiudi.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btChiudi.BackColor = System.Drawing.Color.Transparent;
+            this.btChiudi.BackgroundImage = global::AgendaAziendale.Properties.Resources.Chiudi;
+            this.btChiudi.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btChiudi.Location = new System.Drawing.Point(947, 5);
+            this.btChiudi.Name = "btChiudi";
+            this.btChiudi.Size = new System.Drawing.Size(45, 30);
+            this.btChiudi.TabIndex = 3;
+            this.btChiudi.UseVisualStyleBackColor = false;
+            this.btChiudi.Click += new System.EventHandler(this.BtChiudi_Click);
+            // 
+            // panelSinistra
+            // 
+            this.panelSinistra.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panelSinistra.Controls.Add(this.btLogout);
+            this.panelSinistra.Controls.Add(this.btGestioneUtenti);
+            this.panelSinistra.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelSinistra.Location = new System.Drawing.Point(0, 40);
+            this.panelSinistra.Name = "panelSinistra";
+            this.panelSinistra.Size = new System.Drawing.Size(300, 610);
+            this.panelSinistra.TabIndex = 4;
+            // 
+            // btLogout
+            // 
+            this.btLogout.BackColor = System.Drawing.Color.DarkGray;
+            this.btLogout.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btLogout.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btLogout.ForeColor = System.Drawing.Color.White;
+            this.btLogout.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btLogout.Location = new System.Drawing.Point(25, 500);
+            this.btLogout.Name = "btLogout";
+            this.btLogout.Size = new System.Drawing.Size(255, 55);
+            this.btLogout.TabIndex = 2;
+            this.btLogout.Text = "Logout";
+            this.btLogout.UseVisualStyleBackColor = false;
+            this.btLogout.Click += new System.EventHandler(this.BtLogout_Click);
+            // 
+            // btGestioneUtenti
+            // 
+            this.btGestioneUtenti.BackColor = System.Drawing.Color.DarkGray;
+            this.btGestioneUtenti.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btGestioneUtenti.ForeColor = System.Drawing.Color.White;
+            this.btGestioneUtenti.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btGestioneUtenti.Location = new System.Drawing.Point(50, 85);
+            this.btGestioneUtenti.Name = "btGestioneUtenti";
+            this.btGestioneUtenti.Size = new System.Drawing.Size(200, 35);
+            this.btGestioneUtenti.TabIndex = 0;
+            this.btGestioneUtenti.Text = "Gestione utenti";
+            this.btGestioneUtenti.UseVisualStyleBackColor = false;
+            this.btGestioneUtenti.Click += new System.EventHandler(this.BtGestioneUtenti_Click);
+            // 
+            // panelCentro
+            // 
+            this.panelCentro.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelCentro.Location = new System.Drawing.Point(300, 40);
+            this.panelCentro.Name = "panelCentro";
+            this.panelCentro.Size = new System.Drawing.Size(700, 610);
+            this.panelCentro.TabIndex = 5;
+            // 
+            // FormAdmin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1000, 650);
+            this.Controls.Add(this.panelCentro);
+            this.Controls.Add(this.panelSinistra);
+            this.Controls.Add(this.panelTop);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormAdmin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormAdmin";
+            this.Load += new System.EventHandler(this.FormAdmin_Load);
+            this.panelTop.ResumeLayout(false);
+            this.panelSinistra.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelTop;
+        private System.Windows.Forms.Button btChiudi;
+        private System.Windows.Forms.Panel panelSinistra;
+        private System.Windows.Forms.Button btGestioneUtenti;
+        private System.Windows.Forms.Panel panelCentro;
+        private System.Windows.Forms.Button btLogout;
+    }
+}
---
+++ AgendaAziendale/Forms/FormAdmin.cs
@@ -0,0 +1,104 @@
+﻿using AgendaAziendale.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AgendaAziendale
+{
+    public partial class FormAdmin : Form
+    {
+
+        /// <summary>
+        /// Metodo costruttore del FormLogin
+        /// </summary>
+        public FormAdmin()
+        {
+            InitializeComponent();
+        }
+
+        #region Ascoltatori eventi
+        /// <summary>
+        /// Metodo richiamato al caricamento dell'interfaccia
+        ///  --> settaggio gerarchie interfaccia
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FormAdmin_Load(object sender, EventArgs e)
+        {
+            ///Figli del form
+            panelTop.Parent = this;
+            panelSinistra.Parent = this;
+            panelCentro.Parent = this;
+            ///Figli del pannello top
+            btChiudi.Parent = panelTop;
+            ///Figli del pannello di sinistra
+            btGestioneUtenti.Parent = panelSinistra;
+            btLogout.Parent = panelSinistra;
+        }
+
+        /// <summary>
+        /// Ascoltatore evento click sul bottone di chiusura 
+        /// --> chiusura applicazione
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtChiudi_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        /// <summary>
+        /// Ascoltatore evento click sul bottone di gestione per utenti
+        /// --> visualizzazione UserControl di gestione per gli utenti
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtGestioneUtenti_Click(object sender, EventArgs e)
+        {
+            CaricaForm(new FormGestioneUtenti());
+        }
+
+        /// <summary>
+        /// Ascoltatore evento click sul bottone di logout
+        /// --> effettua il logout, riporta alla schermata di login
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtLogout_Click(object sender, EventArgs e)
+        {
+            FormLogin formLogin = new FormLogin();
+            formLogin.Show();
+            Close(); //Chiudo questo form
+        }
+        #endregion
+
+        #region Metodi
+        /// <summary>
+        /// Metodo adibito al caricamento dinamico dell'interfaccia d'azione nel pannello centrale
+        /// sulla base della selezione dell'utente
+        /// </summary>
+        /// <param name="formIn"></param>
+        public void CaricaForm(object formIn)
+        {
+
+            if (panelCentro.Controls.Count > 0) //Controllo che non ci siano altri form già all'interno dell'interfaccia
+                panelCentro.Controls.RemoveAt(0); //Elimino i controlli relativi al form precedentemente inserito nell'interfaccia
+
+            Form f = formIn as Form;
+            f.TopLevel = false;
+            f.Dock = DockStyle.Fill; //Mi assicuro che occupi tutto lo spazio disponibile, se fosse troppo grande lo ridimensionerebbe
+            panelCentro.Controls.Add(f); //Asssegno i controlli al form "dinamico" creato
+            panelCentro.Tag = f;
+            f.Show();
+        }
+        #endregion
+
+
+    }
+}
