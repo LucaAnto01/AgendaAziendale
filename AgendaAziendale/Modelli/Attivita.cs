2fa41c48043f505e3640e55126654cf97b081715
LucaAnto01 <luca.antognarelli@gmail.com>
2022-04-11 19:38:23 +0200

    Creazione classi Attivita & Lavoratore

---
+++ AgendaAziendale/Modelli/Attivita.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaAziendale.Modelli
+{
+    internal class Attivita
+    {
+        #region Attributi
+        private string codice;
+        private Lavoratore referente;
+        private string nome;
+        private string descrizione;
+        private DateTime dataInizio;
+        private DateTime dataFine;
+        #endregion
+
+        #region Metodi costruttori
+        /// <summary>
+        /// Metodo costruttore della classe Attivita
+        /// </summary>
+        /// <param name="codice"></param>
+        /// <param name="referente"></param>
+        /// <param name="nome"></param>
+        /// <param name="descrizione"></param>
+        /// <param name="dataInizio"></param>
+        /// <param name="dataFine"></param>
+        public Attivita(string codice, Lavoratore referente, string nome, string descrizione, DateTime dataInizio, DateTime dataFine)
+        {
+            this.codice = codice;
+            this.referente = referente;
+            this.nome = nome;
+            this.descrizione = descrizione;
+            this.dataInizio = dataInizio;
+            this.dataFine = dataFine;
+        }
+
+        /// <summary>
+        /// Metodo costruttore vuoto della classe Attivita
+        /// </summary>
+        public Attivita() { }
+        #endregion
+
+        #region Getters & Setters
+        public string Codice { get => codice; set => codice = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public string Descrizione { get => descrizione; set => descrizione = value; }
+        public DateTime DataInizio { get => dataInizio; set => dataInizio = value; }
+        public DateTime DataFine { get => dataFine; set => dataFine = value; }
+        internal Lavoratore Referente { get => referente; set => referente = value; }
+        #endregion
+    }
+}
---
+++ AgendaAziendale/Modelli/Lavoratore.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaAziendale.Modelli
+{
+    internal abstract class Lavoratore
+    {
+        #region Attributi
+        private string username;
+        private string password;
+        private string nome;
+        private string cognome;
+        private string residenza;
+        private DateTime dataNascita;
+        private string email; //Generata automaticamente
+        private string categoria;
+        #endregion
+
+        #region Metodi costruttori
+        /// <summary>
+        /// Metodo costruttore della classe Lavoratore
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <param name="nome"></param>
+        /// <param name="cognome"></param>
+        /// <param name="residenza"></param>
+        /// <param name="dataNascita"></param>
+        /// <param name="email"></param>
+        /// <param name="categoria"></param>
+        public Lavoratore(string username, string password, string nome, string cognome, string residenza, DateTime dataNascita, string email, string categoria)
+        {
+            Username = username;
+            Password = password;
+            Nome = nome;
+            Cognome = cognome;
+            Residenza = residenza;
+            DataNascita = dataNascita;
+            Email = email;
+            Categoria = categoria;
+        }
+
+        /// <summary>
+        /// Metodo costruttore vuoto della classe Lavoratore
+        /// </summary>
+        public Lavoratore() { }
+        #endregion
+
+        #region Getters & Setters
+        public string Username { get => username; set => username = value; }
+        public string Password { get => password; set => password = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public string Cognome { get => cognome; set => cognome = value; }
+        public string Residenza { get => residenza; set => residenza = value; }
+        public DateTime DataNascita { get => dataNascita; set => dataNascita = value; }
+        public string Email { get => email; set => email = value; }
+        public string Categoria { get => categoria; set => categoria = value; }
+        #endregion
+    }
+}
