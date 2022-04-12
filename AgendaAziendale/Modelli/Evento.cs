b978000a6d45bb67a16c9f3e3ba8849de31c89b7
LucaAnto01 <luca.antognarelli@gmail.com>
2022-04-12 16:21:06 +0200

    Creazione classi: Obiettivo, Progetto ed Evento

---
+++ AgendaAziendale/Modelli/Evento.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaAziendale.Modelli
+{
+    internal class Evento : Attivita
+    {
+        #region Attributi
+        private int id;
+        private string luogo;
+
+
+        #endregion
+
+        #region Metodi e costruttori
+        /// <summary>
+        /// Metodo costruttore della classe Evento
+        /// </summary>
+        /// <param name="codice"></param>
+        /// <param name="referente"></param>
+        /// <param name="nome"></param>
+        /// <param name="descrizione"></param>
+        /// <param name="dataInizio"></param>
+        /// <param name="dataFine"></param>
+        /// <param name="id"></param>
+        /// <param name="luogo"></param>
+        public Evento(string codice, Lavoratore referente, string nome, string descrizione, DateTime dataInizio, DateTime dataFine, int id, string luogo) : base(codice, referente, nome, descrizione, dataInizio, dataFine)
+        {
+            Id = id;
+            Luogo = luogo;
+        }
+
+        /// <summary>
+        /// Metodo costruttore vuoto della classe Evento
+        /// </summary>
+        public Evento() { }
+        #endregion
+
+        #region Getters & Setters
+        public int Id { get => id; set => id = value; }
+        public string Luogo { get => luogo; set => luogo = value; }
+        #endregion
+    }
+}
