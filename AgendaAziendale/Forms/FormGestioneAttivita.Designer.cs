0fff3c98b234921ded8ce548c78d9e5a9d090d7b
LucaAnto01 <luca.antognarelli@gmail.com>
2022-04-12 20:19:03 +0200

    Creazione FormGEstioneAttivita

---
+++ AgendaAziendale/Forms/FormGestioneAttivita.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace AgendaAziendale.Forms
+{
+    partial class FormGestioneAttivita
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btCreaEvento = new System.Windows.Forms.Button();
+            this.btCreaProgetto = new System.Windows.Forms.Button();
+            this.btModificaProgetto = new System.Windows.Forms.Button();
+            this.btModificaEvento = new System.Windows.Forms.Button();
+            this.btEliminaProgetto = new System.Windows.Forms.Button();
+            this.btEliminaEvento = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btCreaEvento
+            // 
+            this.btCreaEvento.BackColor = System.Drawing.Color.DarkGray;
+            this.btCreaEvento.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btCreaEvento.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCreaEvento.ForeColor = System.Drawing.Color.White;
+            this.btCreaEvento.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btCreaEvento.Location = new System.Drawing.Point(215, 53);
+            this.btCreaEvento.Name = "btCreaEvento";
+            this.btCreaEvento.Size = new System.Drawing.Size(265, 57);
+            this.btCreaEvento.TabIndex = 2;
+            this.btCreaEvento.Text = "Crea evento";
+            this.btCreaEvento.UseVisualStyleBackColor = false;
+            this.btCreaEvento.Click += new System.EventHandler(this.BtCreaEvento_Click);
+            // 
+            // btCreaProgetto
+            // 
+            this.btCreaProgetto.BackColor = System.Drawing.Color.DarkGray;
+            this.btCreaProgetto.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btCreaProgetto.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCreaProgetto.ForeColor = System.Drawing.Color.White;
+            this.btCreaProgetto.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btCreaProgetto.Location = new System.Drawing.Point(215, 141);
+            this.btCreaProgetto.Name = "btCreaProgetto";
+            this.btCreaProgetto.Size = new System.Drawing.Size(265, 57);
+            this.btCreaProgetto.TabIndex = 3;
+            this.btCreaProgetto.Text = "Crea progetto";
+            this.btCreaProgetto.UseVisualStyleBackColor = false;
+            this.btCreaProgetto.Click += new System.EventHandler(this.BtCreaProgetto_Click);
+            // 
+            // btModificaProgetto
+            // 
+            this.btModificaProgetto.BackColor = System.Drawing.Color.DarkGray;
+            this.btModificaProgetto.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btModificaProgetto.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btModificaProgetto.ForeColor = System.Drawing.Color.White;
+            this.btModificaProgetto.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btModificaProgetto.Location = new System.Drawing.Point(215, 317);
+            this.btModificaProgetto.Name = "btModificaProgetto";
+            this.btModificaProgetto.Size = new System.Drawing.Size(265, 57);
+            this.btModificaProgetto.TabIndex = 5;
+            this.btModificaProgetto.Text = "Modifica progetto";
+            this.btModificaProgetto.UseVisualStyleBackColor = false;
+            this.btModificaProgetto.Click += new System.EventHandler(this.BtModificaProgetto_Click);
+            // 
+            // btModificaEvento
+            // 
+            this.btModificaEvento.BackColor = System.Drawing.Color.DarkGray;
+            this.btModificaEvento.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btModificaEvento.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btModificaEvento.ForeColor = System.Drawing.Color.White;
+            this.btModificaEvento.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btModificaEvento.Location = new System.Drawing.Point(216, 229);
+            this.btModificaEvento.Name = "btModificaEvento";
+            this.btModificaEvento.Size = new System.Drawing.Size(265, 57);
+            this.btModificaEvento.TabIndex = 4;
+            this.btModificaEvento.Text = "Modifica evento";
+            this.btModificaEvento.UseVisualStyleBackColor = false;
+            this.btModificaEvento.Click += new System.EventHandler(this.BtModificaEvento_Click);
+            // 
+            // btEliminaProgetto
+            // 
+            this.btEliminaProgetto.BackColor = System.Drawing.Color.DarkGray;
+            this.btEliminaProgetto.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btEliminaProgetto.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btEliminaProgetto.ForeColor = System.Drawing.Color.White;
+            this.btEliminaProgetto.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btEliminaProgetto.Location = new System.Drawing.Point(215, 493);
+            this.btEliminaProgetto.Name = "btEliminaProgetto";
+            this.btEliminaProgetto.Size = new System.Drawing.Size(265, 57);
+            this.btEliminaProgetto.TabIndex = 9;
+            this.btEliminaProgetto.Text = "Elimina progetto";
+            this.btEliminaProgetto.UseVisualStyleBackColor = false;
+            this.btEliminaProgetto.Click += new System.EventHandler(this.BtEliminaProgetto_Click);
+            // 
+            // btEliminaEvento
+            // 
+            this.btEliminaEvento.BackColor = System.Drawing.Color.DarkGray;
+            this.btEliminaEvento.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btEliminaEvento.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btEliminaEvento.ForeColor = System.Drawing.Color.White;
+            this.btEliminaEvento.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btEliminaEvento.Location = new System.Drawing.Point(215, 405);
+            this.btEliminaEvento.Name = "btEliminaEvento";
+            this.btEliminaEvento.Size = new System.Drawing.Size(265, 57);
+            this.btEliminaEvento.TabIndex = 8;
+            this.btEliminaEvento.Text = "Elimina evento";
+            this.btEliminaEvento.UseVisualStyleBackColor = false;
+            this.btEliminaEvento.Click += new System.EventHandler(this.BtEliminaEvento_Click);
+            // 
+            // FormGestioneAttivita
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(700, 610);
+            this.Controls.Add(this.btEliminaProgetto);
+            this.Controls.Add(this.btEliminaEvento);
+            this.Controls.Add(this.btModificaProgetto);
+            this.Controls.Add(this.btModificaEvento);
+            this.Controls.Add(this.btCreaProgetto);
+            this.Controls.Add(this.btCreaEvento);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormGestioneAttivita";
+            this.Text = "FormGestioneAttivita";
+            this.Load += new System.EventHandler(this.FormGestioneAttivita_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btCreaEvento;
+        private System.Windows.Forms.Button btCreaProgetto;
+        private System.Windows.Forms.Button btModificaProgetto;
+        private System.Windows.Forms.Button btModificaEvento;
+        private System.Windows.Forms.Button btEliminaProgetto;
+        private System.Windows.Forms.Button btEliminaEvento;
+    }
+}
---
+++ AgendaAziendale/Forms/FormGestioneAttivita.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AgendaAziendale.Modelli;
+
+namespace AgendaAziendale.Forms
+{
+    public partial class FormGestioneAttivita : Form
+    {
+
+        private Lavoratore lavoratore;
+
+        #region Getters & Setters
+        public Lavoratore Lavoratore { get => lavoratore; set => lavoratore = value; }
+        #endregion
+
+        /// <summary>
+        /// Metodo costruttore del FormGestioneAttivita
+        /// </summary>
+        /// <param name="lavoratore"></param>
+        public FormGestioneAttivita(Lavoratore lavoratore)
+        {
+            InitializeComponent();
+            Lavoratore = lavoratore;
+        }
+
+        #region Ascoltatori eventi
+        /// <summary>
+        /// Metodo richiamato al caricamento dell'interfaccia
+        ///  --> settaggio gerarchie interfaccia
+        ///  --> settaggio visibilità bottoni per relativa possibilità d'azione
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FormGestioneAttivita_Load(object sender, EventArgs e)
+        {
+            //TODO: controlla il tipo di lavoratore per rendere visibile o meno certi bottoni
+        }
+
+        /// <summary>
+        /// Ascoltatore click sul bottone per creazione evento
+        /// --> apertura Form adibito alla creazione dell'evento
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtCreaEvento_Click(object sender, EventArgs e)
+        {
+            FormCreazioneEvento formCreaEvento = new FormCreazioneEvento();
+            formCreaEvento.Show();
+        }
+
+        /// <summary>
+        /// Ascoltatore click sul bottone per creazione progetto
+        /// --> apertura Form adibito alla creazione dell'progetto
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtCreaProgetto_Click(object sender, EventArgs e)
+        {
+            //TODO: crea form per creazione progetto
+        }
+
+        /// <summary>
+        /// Ascoltatore click sul bottone per modifica evento
+        /// --> apertura Form adibito alla modifica di un evento
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtModificaEvento_Click(object sender, EventArgs e)
+        {
+            //TODO: crea form per modifica evento
+        }
+
+        /// <summary>
+        /// Ascoltatore click sul bottone per modifica progetto
+        /// --> apertura Form adibito alla modifica di un progetto
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtModificaProgetto_Click(object sender, EventArgs e)
+        {
+            //TODO: crea form per modifica progetto
+        }
+
+        /// <summary>
+        ///  Ascoltatore click sul bottone per eliminazione evento
+        /// --> apertura Form adibito all'eliminazione di un evento
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtEliminaEvento_Click(object sender, EventArgs e)
+        {
+            //TODO: crea form per eliminazione evento
+        }
+
+        /// <summary>
+        /// Ascoltatore click sul bottone per eliminazione progetto
+        /// --> apertura Form adibito all'eliminazione di un progetto
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtEliminaProgetto_Click(object sender, EventArgs e)
+        {
+            //TODO: crea form per eliminazione progetto
+        }
+        #endregion
+    }
+}
