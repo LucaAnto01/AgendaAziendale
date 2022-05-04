01bfc2932fb63d97886312785d2cf4b2e576dc23
LucaAnto01 <luca.antognarelli@gmail.com>
2022-05-04 22:52:58 +0200

    Creazione ed implementazione UC personalizzati

---
+++ AgendaAziendale/Forms/UserControls/UCLavoratori.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace AgendaAziendale.Forms.UserControls
+{
+    partial class UCLavoratori
+    {
+        /// <summary> 
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione componenti
+
+        /// <summary> 
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare 
+        /// il contenuto del metodo con l'editor di id.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.panelCentro = new System.Windows.Forms.Panel();
+            this.dgvLavoratori = new System.Windows.Forms.DataGridView();
+            this.username = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cognome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.residenza = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.data_nascita = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.email = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.categoria = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.modifica = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.elimina = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.panelCentro.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLavoratori)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelCentro
+            // 
+            this.panelCentro.BackColor = System.Drawing.Color.White;
+            this.panelCentro.Controls.Add(this.dgvLavoratori);
+            this.panelCentro.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelCentro.Location = new System.Drawing.Point(0, 0);
+            this.panelCentro.Name = "panelCentro";
+            this.panelCentro.Size = new System.Drawing.Size(645, 445);
+            this.panelCentro.TabIndex = 0;
+            // 
+            // dgvLavoratori
+            // 
+            this.dgvLavoratori.AllowUserToAddRows = false;
+            this.dgvLavoratori.AllowUserToDeleteRows = false;
+            this.dgvLavoratori.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvLavoratori.BackgroundColor = System.Drawing.Color.White;
+            this.dgvLavoratori.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvLavoratori.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLavoratori.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.username,
+            this.nome,
+            this.cognome,
+            this.residenza,
+            this.data_nascita,
+            this.email,
+            this.categoria,
+            this.modifica,
+            this.elimina});
+            this.dgvLavoratori.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvLavoratori.GridColor = System.Drawing.Color.White;
+            this.dgvLavoratori.Location = new System.Drawing.Point(0, 0);
+            this.dgvLavoratori.Name = "dgvLavoratori";
+            this.dgvLavoratori.ReadOnly = true;
+            this.dgvLavoratori.Size = new System.Drawing.Size(645, 445);
+            this.dgvLavoratori.TabIndex = 0;
+            this.dgvLavoratori.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DgvLavoratori_CellClick);
+            // 
+            // username
+            // 
+            this.username.HeaderText = "Username";
+            this.username.Name = "username";
+            this.username.ReadOnly = true;
+            // 
+            // nome
+            // 
+            this.nome.HeaderText = "Nome";
+            this.nome.Name = "nome";
+            this.nome.ReadOnly = true;
+            // 
+            // cognome
+            // 
+            this.cognome.HeaderText = "Cognome";
+            this.cognome.Name = "cognome";
+            this.cognome.ReadOnly = true;
+            // 
+            // residenza
+            // 
+            this.residenza.HeaderText = "Residenza";
+            this.residenza.Name = "residenza";
+            this.residenza.ReadOnly = true;
+            // 
+            // data_nascita
+            // 
+            this.data_nascita.HeaderText = "DataNascita";
+            this.data_nascita.Name = "data_nascita";
+            this.data_nascita.ReadOnly = true;
+            // 
+            // email
+            // 
+            this.email.HeaderText = "Email";
+            this.email.Name = "email";
+            this.email.ReadOnly = true;
+            // 
+            // categoria
+            // 
+            this.categoria.HeaderText = "Categoria";
+            this.categoria.Name = "categoria";
+            this.categoria.ReadOnly = true;
+            // 
+            // modifica
+            // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI Historic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.White;
+            this.modifica.DefaultCellStyle = dataGridViewCellStyle1;
+            this.modifica.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.modifica.HeaderText = "";
+            this.modifica.Name = "modifica";
+            this.modifica.ReadOnly = true;
+            this.modifica.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.modifica.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.modifica.Text = "Modifica";
+            this.modifica.UseColumnTextForButtonValue = true;
+            // 
+            // elimina
+            // 
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI Historic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.White;
+            this.elimina.DefaultCellStyle = dataGridViewCellStyle2;
+            this.elimina.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.elimina.HeaderText = "";
+            this.elimina.Name = "elimina";
+            this.elimina.ReadOnly = true;
+            this.elimina.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.elimina.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.elimina.Text = "Elimina";
+            this.elimina.UseColumnTextForButtonValue = true;
+            // 
+            // UCLavoratori
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.panelCentro);
+            this.Name = "UCLavoratori";
+            this.Size = new System.Drawing.Size(645, 445);
+            this.Load += new System.EventHandler(this.UCLavoratori_Load);
+            this.panelCentro.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLavoratori)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelCentro;
+        private System.Windows.Forms.DataGridView dgvLavoratori;
+        private System.Windows.Forms.DataGridViewTextBoxColumn username;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cognome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn residenza;
+        private System.Windows.Forms.DataGridViewTextBoxColumn data_nascita;
+        private System.Windows.Forms.DataGridViewTextBoxColumn email;
+        private System.Windows.Forms.DataGridViewTextBoxColumn categoria;
+        private System.Windows.Forms.DataGridViewButtonColumn modifica;
+        private System.Windows.Forms.DataGridViewButtonColumn elimina;
+    }
+}
