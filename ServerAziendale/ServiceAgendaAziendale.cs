abe392623910e1f656d12fcec67068976e291d42
LucaAnto01 <luca.antognarelli@gmail.com>
2022-05-10 14:19:12 +0200

    Creazione servizio agenda aziendale

---
+++ ServerAziendale/ServiceAgendaAziendale.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServerAziendale
+{
+    // NOTA: è possibile utilizzare il comando "Rinomina" del menu "Refactoring" per modificare il nome di classe "ServiceAgendaAziendale" nel codice e nel file di configurazione contemporaneamente.
+    public class ServiceAgendaAziendale : IServiceAgendaAziendale
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
