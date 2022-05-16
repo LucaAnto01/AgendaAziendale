8dab4cfc6e6a25135064680a38bb9e54c61d05e6
LucaAnto01 <luca.antognarelli@gmail.com>
2022-05-16 16:00:05 +0200

    Creazione classe Sessione

---
+++ AgendaAziendale/Modelli/Sessione.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaAziendale.Modelli
+{
+    /// <summary>
+    /// Classe statica adibita all'utilizzo di una simil-sessione
+    /// I dati vengono settati una volta effettuata la fase di login
+    /// </summary>
+    public static class Sessione
+    {
+        public static string username;
+        public static string password;
+    }
+}
