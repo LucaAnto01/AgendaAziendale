ed6b39095b9580a45464f0204888949a9010a2cb
LucaAnto01 <luca.antognarelli@gmail.com>
2022-05-17 18:12:55 +0200

    Creazione FormObiettivo

---
+++ AgendaAziendale/Forms/FormObiettivo.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace AgendaAziendale.Forms
+{
+    partial class FormObiettivo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btChiudi = new System.Windows.Forms.Button();
+            this.panelTop = new System.Windows.Forms.Panel();
+            this.panelCentro = new System.Windows.Forms.Panel();
+            this.tbDescrizione = new System.Windows.Forms.TextBox();
+            this.lbDescrizione = new System.Windows.Forms.Label();
+            this.lbCompletato = new System.Windows.Forms.Label();
+            this.ckCompletato = new System.Windows.Forms.CheckBox();
+            this.btAggiungiAggiorna = new System.Windows.Forms.Button();
+            this.panelTop.SuspendLayout();
+            this.panelCentro.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btChiudi
+            // 
+            this.btChiudi.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btChiudi.BackColor = System.Drawing.Color.Transparent;
+            this.btChiudi.BackgroundImage = global::AgendaAziendale.Properties.Resources.Chiudi;
+            this.btChiudi.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btChiudi.Location = new System.Drawing.Point(461, 5);
+            this.btChiudi.Name = "btChiudi";
+            this.btChiudi.Size = new System.Drawing.Size(45, 30);
+            this.btChiudi.TabIndex = 3;
+            this.btChiudi.UseVisualStyleBackColor = false;
+            this.btChiudi.Click += new System.EventHandler(this.BtChiudi_Click);
+            // 
+            // panelTop
+            // 
+            this.panelTop.BackColor = System.Drawing.Color.Gainsboro;
+            this.panelTop.Controls.Add(this.btChiudi);
+            this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTop.Location = new System.Drawing.Point(0, 0);
+            this.panelTop.Name = "panelTop";
+            this.panelTop.Size = new System.Drawing.Size(514, 40);
+            this.panelTop.TabIndex = 68;
+            // 
+            // panelCentro
+            // 
+            this.panelCentro.Controls.Add(this.btAggiungiAggiorna);
+            this.panelCentro.Controls.Add(this.ckCompletato);
+            this.panelCentro.Controls.Add(this.lbCompletato);
+            this.panelCentro.Controls.Add(this.tbDescrizione);
+            this.panelCentro.Controls.Add(this.lbDescrizione);
+            this.panelCentro.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelCentro.Location = new System.Drawing.Point(0, 40);
+            this.panelCentro.Name = "panelCentro";
+            this.panelCentro.Size = new System.Drawing.Size(514, 402);
+            this.panelCentro.TabIndex = 69;
+            // 
+            // tbDescrizione
+            // 
+            this.tbDescrizione.AcceptsReturn = true;
+            this.tbDescrizione.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbDescrizione.Location = new System.Drawing.Point(119, 48);
+            this.tbDescrizione.MaxLength = 500;
+            this.tbDescrizione.Multiline = true;
+            this.tbDescrizione.Name = "tbDescrizione";
+            this.tbDescrizione.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbDescrizione.Size = new System.Drawing.Size(246, 46);
+            this.tbDescrizione.TabIndex = 72;
+            // 
+            // lbDescrizione
+            // 
+            this.lbDescrizione.AutoSize = true;
+            this.lbDescrizione.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDescrizione.Location = new System.Drawing.Point(12, 48);
+            this.lbDescrizione.Name = "lbDescrizione";
+            this.lbDescrizione.Size = new System.Drawing.Size(93, 18);
+            this.lbDescrizione.TabIndex = 71;
+            this.lbDescrizione.Text = "Descrizione";
+            // 
+            // lbCompletato
+            // 
+            this.lbCompletato.AutoSize = true;
+            this.lbCompletato.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCompletato.Location = new System.Drawing.Point(12, 156);
+            this.lbCompletato.Name = "lbCompletato";
+            this.lbCompletato.Size = new System.Drawing.Size(89, 18);
+            this.lbCompletato.TabIndex = 73;
+            this.lbCompletato.Text = "Completato";
+            // 
+            // ckCompletato
+            // 
+            this.ckCompletato.AutoSize = true;
+            this.ckCompletato.Location = new System.Drawing.Point(119, 158);
+            this.ckCompletato.Name = "ckCompletato";
+            this.ckCompletato.Size = new System.Drawing.Size(79, 17);
+            this.ckCompletato.TabIndex = 74;
+            this.ckCompletato.Text = "Completato";
+            this.ckCompletato.UseVisualStyleBackColor = true;
+            // 
+            // btAggiungiAggiorna
+            // 
+            this.btAggiungiAggiorna.Location = new System.Drawing.Point(15, 227);
+            this.btAggiungiAggiorna.Name = "btAggiungiAggiorna";
+            this.btAggiungiAggiorna.Size = new System.Drawing.Size(75, 23);
+            this.btAggiungiAggiorna.TabIndex = 75;
+            this.btAggiungiAggiorna.Text = "Agg";
+            this.btAggiungiAggiorna.UseVisualStyleBackColor = true;
+            this.btAggiungiAggiorna.Click += new System.EventHandler(this.BtAggiungiAggiorna_Click);
+            // 
+            // FormObiettivo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(514, 442);
+            this.Controls.Add(this.panelCentro);
+            this.Controls.Add(this.panelTop);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormObiettivo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormObiettivo";
+            this.Load += new System.EventHandler(this.FormObiettivo_Load);
+            this.panelTop.ResumeLayout(false);
+            this.panelCentro.ResumeLayout(false);
+            this.panelCentro.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btChiudi;
+        private System.Windows.Forms.Panel panelTop;
+        private System.Windows.Forms.Panel panelCentro;
+        private System.Windows.Forms.TextBox tbDescrizione;
+        private System.Windows.Forms.Label lbDescrizione;
+        private System.Windows.Forms.CheckBox ckCompletato;
+        private System.Windows.Forms.Label lbCompletato;
+        private System.Windows.Forms.Button btAggiungiAggiorna;
+    }
+}
---
+++ AgendaAziendale/Forms/FormObiettivo.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AgendaAziendale.Forms.UserControls;
+using AgendaAziendale.Modelli;
+
+namespace AgendaAziendale.Forms
+{
+    public partial class FormObiettivo : Form
+    {
+        #region Attributi
+        private Form formPadre;
+        private readonly UCProgetti ucPadre;
+        private readonly Obiettivo obiettivo;
+        private readonly string azione;
+        #endregion
+
+        #region Getters & Setters
+        public Form FormPadre { get => formPadre; set => formPadre = value; }
+        #endregion
+
+        #region Metodi costruttori
+        /// <summary>
+        /// Metodo costruttore del FormObiettivo
+        /// </summary>
+        /// <param name="formPadre"></param>
+        /// <param name="ucPadre"></param>
+        /// <param name="obiettivo"></param>
+        /// <param name="azione"></param>
+        public FormObiettivo(Form formPadre, UCProgetti ucPadre, Obiettivo obiettivo, string azione)
+        {
+            InitializeComponent();
+            FormPadre = formPadre;
+            this.ucPadre = ucPadre;
+            this.obiettivo = obiettivo;
+            this.azione = azione;
+        }
+
+        /// <summary>
+        /// Metodo costruttore del FormObiettivo
+        /// </summary>
+        /// <param name="formPadre"></param>
+        /// <param name="ucPadre"></param>
+        /// <param name="azione"></param>
+        public FormObiettivo(Form formPadre, UCProgetti ucPadre, string azione)
+        {
+            InitializeComponent();
+            FormPadre = formPadre;
+            this.ucPadre = ucPadre;
+            this.obiettivo = null;
+            this.azione = azione;
+        }
+        #endregion
+
+        #region Ascoltatori eventi
+        /// <summary>
+        /// Metodo richiamato al caricamento dell'interfaccia
+        ///  --> settaggio gerarchie interfaccia
+        ///  --> settaggio testi
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FormObiettivo_Load(object sender, EventArgs e)
+        {
+            ///Figli del Form
+            panelTop.Parent = this;
+            panelCentro.Parent = this;
+            ///Figli del pannello top
+            btChiudi.Parent = panelTop;
+            ///Figli del pannello centrale
+            lbDescrizione.Parent = panelCentro;
+            lbCompletato.Parent = panelCentro;
+            tbDescrizione.Parent = panelCentro;
+            ckCompletato.Parent = panelCentro;
+            btAggiungiAggiorna.Parent = panelCentro;
+
+            if(obiettivo != null) //Se ho un obiettivo da modificare
+            {
+                tbDescrizione.Text = obiettivo.Desccrizione;
+
+                if(obiettivo.Completato)
+                    ckCompletato.Checked = true;
+            }
+
+            if ((azione == "Aggiungi") || (azione == "aggiungi"))
+                btAggiungiAggiorna.Text = "Aggiungi";
+
+            else if ((azione == "Aggiorna") || (azione == "aggiorna"))
+                btAggiungiAggiorna.Text = "Aggiorna";
+
+            else
+            {
+                MessageBox.Show("ERRORE! Valore FormObiettivo:tipologia --> controllare stack chiamate!", "Compilazione campi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+
+            FormPadre.Hide();
+        }
+
+        /// <summary>
+        /// Ascoltatore evento click sul bottone di chiusura 
+        /// --> chiusura form
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtChiudi_Click(object sender, EventArgs e)
+        {
+            FormPadre.ShowDialog();
+            Close();
+        }
+
+        /// <summary>
+        /// Ascoltatore click sul bottone adibito alla creazione o all'aggiornamento di un obiettivo e del relativo progetto di conseguenza
+        /// --> effettuo l'azione di aggiunta o modifica
+        /// --> aggiorno la dgv contenente i dati
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtAggiungiAggiorna_Click(object sender, EventArgs e)
+        {
+            if ((azione == "Aggiungi") || (azione == "aggiungi"))
+                //Richiama funzione aggiungi
+
+            if ((azione == "Aggiorna") || (azione == "aggiorna"))
+                //Richiama funzione aggiorna
+
+            if (ucPadre != null)
+                ucPadre.AggiornadgvProgetti();
+        }
+        #endregion
+    }
+}
